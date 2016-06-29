49a58939475523004d26f7989765e3be746039aa
CochranBC <briancochran85@gmail.com>
2016-06-29 16:42:16 -0700

    Forms almost completed.

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json();
+
+app.use(jsonParser);
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/default.js', function (req, res) {
+  res.sendFile(__dirname +  '/default.js')
+});
+
+
+var golfers = [
+  {
+    firstName: 'Bob',
+    lastName: 'Barker',
+    email: 'bbarker@gmail.com',
+    password: 'thepriceisright',
+    city: 'Pitt Meadows',
+    state: 'British Columbia',
+    zip: '92037',
+    gender: 'male',
+    birthday: 'December 12 1923',
+    skill: 'Professional',
+    handicap: '3'
+  },
+  {
+    firstName: 'Adam',
+    lastName: 'Sandler',
+    email: 'asandler@gmail.com',
+    password: 'thepriceiswrong',
+    city: 'Pitt Meadows',
+    state: 'British Columbia',
+    zip: '92037',
+    gender: 'male',
+    birthday: 'September 9 1966',
+    skill: 'Intermediate',
+    handicap: '12'
+  }
+];
+
+
+
+app.listen(8080)
--- default.js
+++ default.js
@@ -1,5 +1,21 @@
-var loginButton = document.getElementByID('login-button');
 
-loginButton.addEventListener('click', function() {
-  var username = document
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function swap(current, next, location) {
+  var old = document.getElementsByClassName(current)[0];
+  old.classList.remove('current');
+  old.classList.add('hide');
+};
